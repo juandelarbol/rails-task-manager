98bd94e076aec86a6e99aa2a30a96c5d33bbbeb3
Jean Delarbre <delarbrejean@icloud.com>
2024-11-12 15:55:47 +0100

    done !

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,15 +12,33 @@ class TasksController < ApplicationController
   end
 
   def create
+
     @task = Task.new(task_params)
+    raise
     @task.save # Will raise ActiveModel::ForbiddenAttributesError
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "tasks#index"
+  
+  #Create
   get "/tasks/new", to: "tasks#new"
   post "tasks", to: "tasks#create"
 
+  #Read
   get "/tasks", to: "tasks#index"
   get "/tasks/:id", to: "tasks#show", as: "task"
+
+  #Update
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
+
+  #Delete
+  delete "/tasks/:id", to: "tasks#destroy"
+
 
 end
